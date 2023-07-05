6d59fc95b9daee48145f381c093916ae184e2683
salvation <savvyplus020@gmail.com>
2023-07-05 23:39:10 +0100

    Added logic to announce winner after 3 rounds

--- main.js
+++ main.js
@@ -1,74 +1,3 @@
-/* let count = 0;
-while (count < 3) {
-  function playRound(player, computer) {
-    let computerScore = 0;
-    let playerScore = 0;
-    if (player === computer) {
-      console.log("Draw");
-    } else if (player === "paper" && computer === "rock") {
-      console.log("you won");
-      playerScore += 1;
-      count += 1;
-    } else if (player === "rock" && computer === "scissors") {
-      console.log("you won");
-      playerScore += 1;
-      count += 1;
-    } else if (player === "scissors" && computer === "paper") {
-      console.log("you won");
-      playerScore += 1;
-      count += 1;
-    } else if (player === "rock" && computer === "paper") {
-      console.log("computer won");
-      computerScore += 1;
-      count += 1;
-    } else if (player === "scissors" && computer === "rock") {
-      console.log("computer won");
-      computerScore += 1;
-      count += 1;
-    } else if (player === "paper" && computer === "scissors") {
-      console.log("computer won");
-      computerScore += 1;
-      count += 1;
-    } else if (player === null || " ") {
-      alert(
-        "please choose between the valid options - rock', 'paper', 'scissors'"
-      );
-    } else {
-      console.log("Sorry the game crashed. Try again!");
-    }
-
-    if (computerScore > playerScore) {
-      console.log("you loose");
-    }else if (computerScore < playerScore) {
-      console.log("you won");
-    }
-  }
-}
-
-function getComputerChoice() {
-  const choice = ["rock", "paper", "scissors"];
-  const random = Math.floor(Math.random() * choice.length);
-  return choice[random];
-}
-
-function getPlayerChoice() {
-  const player = prompt("choose between rock paper and scissors");
-  if (player !== null) {
-    return player.toLowerCase();
-  } else {
-    console.log("player's result is null");
-  }
-} */
-
-//playRound(getPlayerChoice(), getComputerChoice());
-
-/*
-playRound(playerSelection, computerSelection);
-console.log (`player: ${playerSelection}`)
-console.log (`computer: ${computerSelection}`) */
-
-// push to a branch with "git push origin <branch name>"
-
 /* computer choice */
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissors"];
@@ -127,4 +56,6 @@ if (computerScore > playerScore) {
   console.log("you loose");
 } else if (computerScore < playerScore) {
   console.log("you won");
+} else {
+  console.log("it's a tie game!");
 }
